57a9417656f5e33017614c7b00d4759a3cf3ab58
sandy <sandeshjadhav2002@gmail.com>
2024-06-21 23:54:53 +0530

    Feat: Footer add

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image"
+import Link from "next/link"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className=" border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href='/'>
+        {/* <Image
+        src={}
+        /> */}
+        <h3 className="text-xl font-bold">WeHost</h3>
+        </Link>
+        <p>2024 WeHost. All rights reserved.</p>
+      </div>
+    </footer>
   )
 }
 
