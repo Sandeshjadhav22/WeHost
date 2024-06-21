f3b1d62d163de7e4fe340b853c73a8e5eabb8e4f
sandy <sandeshjadhav2002@gmail.com>
2024-06-21 23:49:13 +0530

    Header done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
-import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
+import "./globals.css";
 const poppins = Poppins({
    subsets: ["latin"],
    weight: ['400','500','600','700'],
@@ -23,8 +24,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
+
     <html lang="en">
       <body className={poppins.variable}>{children}</body>
     </html>
+    </ClerkProvider>
   );
 }
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,8 +1,37 @@
+import {  SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import NavItems from "./NavItems";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <h2 className="text-xl font-bold">We Host</h2>
+        </Link>
+         
+         <SignedIn>
+          <nav className="md:flex-between hidden w-full  max-w-xs">
+          <NavItems/>
+          </nav>
+         </SignedIn>
 
-export default Header
+        <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/"/>
+            <MobileNav/>
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full " size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
