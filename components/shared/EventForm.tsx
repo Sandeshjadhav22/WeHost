76c344c51407f40867662b8ba108bcef69fd4c5f
sandy <sandeshjadhav2002@gmail.com>
2024-07-01 21:36:53 +0530

    file Uploader

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -17,6 +17,11 @@ import { Input } from "@/components/ui/input";
 import { eventFormSchema } from "@/lib/validator";
 import { eventDefaultValues } from "@/constants";
 import Dropdown from "./Dropdown";
+import { Textarea } from "@/components/ui/textarea"
+import { FileUploader } from "./FileUploader";
+import { useState } from "react";
+import Image from "next/image";
+
 
 type EventFormProps = {
   userId: string;
@@ -24,6 +29,7 @@ type EventFormProps = {
 };
 
 const EventForm = ({ userId, type }: EventFormProps) => {
+  const [files, setFiles] = useState<File[]>([]);
   const initialValues = eventDefaultValues;
 
   const form = useForm<z.infer<typeof eventFormSchema>>({
@@ -77,6 +83,98 @@ const EventForm = ({ userId, type }: EventFormProps) => {
             )}
           />
         </div>
+           
+           <div className="flex flex-col gap-5 md:flex-row">
+           <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl className="h-72">
+                  <Textarea
+                    placeholder="Description"
+                    {...field}
+                    className="textarea rounded-2xl"
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+           <FormField
+            control={form.control}
+            name="imageUrl"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl className="h-72">
+                 <FileUploader 
+                 onFieldChange={field.onChange}
+                 imageUrl={field.value}
+                 setFiles={setFiles}
+                 />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+           </div> 
+
+           <div className="flex flex-col gap-5 md:flex-row">
+           <FormField
+            control={form.control}
+            name="location"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-gray-50 px-4 py-2">
+                    <Image
+                    src="/assets/icons/location-grey.svg"
+                    alt="calender"
+                    width={24}
+                    height={24}
+                    />
+                  <Input
+                    placeholder="Event location or Online"
+                    {...field}
+                    className="input-field"
+                    />
+                    </div>
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+           </div>
+           <div className="flex flex-col gap-5 md:flex-row">
+           <FormField
+            control={form.control}
+            name="location"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-gray-50 px-4 py-2">
+                    <Image
+                    src="/assets/icons/location-grey.svg"
+                    alt="calender"
+                    width={24}
+                    height={24}
+                    />
+                  <Input
+                    placeholder="Event location or Online"
+                    {...field}
+                    className="input-field"
+                    />
+                    </div>
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+           </div>
+
         <Button type="submit">Submit</Button>
       </form>
     </Form>
