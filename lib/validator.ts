94d0a468e2fa562186a4b437c3ace07e1309d0c8
sandy <sandeshjadhav2002@gmail.com>
2024-07-02 01:30:49 +0530

    from submission & action

--- lib/validator.ts
+++ lib/validator.ts
@@ -3,7 +3,7 @@ import * as z from "zod"
 export const eventFormSchema = z.object({
    title: z.string().min(3,"Title must be atleadt 3 characters"),
    description: z.string().min(3,"descrition must be atleadt 3 characters").max(400,"Description must be less than 400 characters"),
-   location: z.string().max(3,"Location must be 3 characters").max(400,"location must be 400 characterrs"),
+   location: z.string().min(3,"Location must be 3 characters").max(400,"location must be 400 characterrs"),
    imageUrl: z.string(),
    startDateTime: z.date(),
    endDateTime: z.date(),
